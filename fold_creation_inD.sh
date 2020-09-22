125cbf5265cb4fb81036b763e06d403e3ad4ae24
C.B.YE <907450408@qq.com>
2020-09-22 14:18:05 +1000

    fix the fold_creation_inD.sh

--- fold_creation_inD.sh
+++ fold_creation_inD.sh
@@ -1,1 +1,1 @@
-python ./prepare_folds.py --filelist filelist_inD_short.txt --numfolds 1 --output ./fold/3dmot_short --input_length 10 --hidden_dim 10 --singleset
+python ./prepare_folds.py --filelist filelist_inD_short.txt --numfolds 1 --output ./fold/inD_short --input_length 10 --hidden_dim 10 --singleset
