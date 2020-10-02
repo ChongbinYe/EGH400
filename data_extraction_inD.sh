bc8ec6687b308020543b8cbf7ca4dacbc0277b08
C.B.YE <907450408@qq.com>
2020-10-03 05:15:21 +1000

    fix data_extraction_inD.sh

--- data_extraction_inD.sh
+++ data_extraction_inD.sh
@@ -1,1 +1,1 @@
-python ./prepare_data.py --mode 0 --primary_mode ./inD_test_10.pkl --datasource 1 --decimate 1 --length 4 --output ./data/inD_test_10.pkl --windowstep 1 --slidinglimit 10 
+python ./prepare_data.py --mode 0 --primary_mode ./inD_test.pkl --datasource 1 --decimate 1 --length 10 --output ./data/inD_test_10.pkl --windowstep 1 --slidinglimit 10
